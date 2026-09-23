adf3a29faf2f2207ea0fc461822870647726a911
chrehall68 <chrehall68@gmail.com>
2026-09-22 21:40:41 -0700

    graphs (small to large)

--- advancedgraph/transferspeedsums.cpp
+++ advancedgraph/transferspeedsums.cpp
@@ -1,107 +1,126 @@
 #pragma GCC optimize("O3", "unroll-loops")
 #include <bits/stdc++.h>
-// currently too slow
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 
 using namespace std;
 using ll = long long;
 
-// dynamic segtree
-// aka sparse segtree
-class DynamicSegtree {
-  using NodePtr = int;
-  const static NodePtr NULLPTR = -1;
-  struct Node {
-    int left, right; // inclusive
-    NodePtr l, r;
-    ll count;
-    ll weightedSum; // count(1)*1 + count(2)*2 + ...
+pair<ll, ll> &operator+=(pair<ll, ll> &a, const pair<ll, ll> &b) {
+  a.first += b.first;
+  a.second += b.second;
+  return a;
+}
 
-    Node(int left, int right)
-        : left(left), right(right), l(NULLPTR), r(NULLPTR), count(0),
-          weightedSum(0) {}
-  };
-
-  vector<Node> nodes;
-  NodePtr insertNode(Node n) {
-    nodes.push_back(n);
-    return nodes.size() - 1;
-  }
-
-  Node ensureChildren(NodePtr p) {
-    Node node = nodes[p];
-    if (node.l == NULLPTR) {
-      assert(node.r == NULLPTR);
-      int cnt = node.right - node.left + 1;
-      int halfAmt = cnt / 2;
-      nodes[p].l = insertNode(Node(node.left, node.left + halfAmt - 1));
-      nodes[p].r = insertNode(Node(node.left + halfAmt, node.right));
-    }
-    return nodes[p];
-  }
-
-  // returns {count, weightedSum}
-  pair<ll, ll> query(NodePtr p, int queryL, int queryR) {
-    Node node = nodes[p];
-    if (queryL <= node.left && node.right <= queryR) {
-      return {node.count, node.weightedSum};
-    } else if (node.right < queryL || queryR < node.left) {
-      return {0, 0};
-    }
-    node = ensureChildren(p);
-    auto [c1, w1] = query(node.l, queryL, queryR);
-    auto [c2, w2] = query(node.r, queryL, queryR);
-    return {c1 + c2, w1 + w2};
-  }
-
-  void increment(NodePtr p, int num, ll cnt) {
-    Node node = nodes[p];
-    if (node.left == num && node.right == num) {
-      // made it to a leaf
-      nodes[p].count += cnt;
-      nodes[p].weightedSum += cnt * num;
-    } else if (node.left > num || node.right < num) {
-      return; // out of range
-    } else {
-      // in one of children
-      node = ensureChildren(p);
-      increment(node.l, num, cnt);
-      increment(node.r, num, cnt);
-      // and pull updates to my values
-      nodes[p].count = nodes[node.l].count + nodes[node.r].count;
-      nodes[p].weightedSum =
-          nodes[node.l].weightedSum + nodes[node.r].weightedSum;
-    }
-  }
-
-  NodePtr root;
-
+// __gnu_pbds is a very fast implementation of a tree
+// and allows us to store custom metadata in each tree node and
+// define methods that can be called
+template <class Node_CItr, class Node_Itr, class Cmp_Fn, class _Alloc>
+class custom_node_update
+    : public tree_order_statistics_node_update<Node_CItr, Node_Itr, Cmp_Fn,
+                                               _Alloc> {
 public:
-  DynamicSegtree(int left, int right) { root = insertNode(Node(left, right)); }
+  // {count, weightedSum}
+  typedef pair<ll, ll> metadata_type;
 
-  void increment(int num, ll cnt) { increment(root, num, cnt); }
-  pair<ll, ll> query(int left, int right) { return query(root, left, right); }
+  // note: just using int and ll raw since that's what this
+  // will be specialized as
+  ll extract_value(Node_CItr it) { return (*it)->second; }
+  int extract_key(Node_CItr it) { return (*it)->first; }
+
+  // value for just this {key, cnt} pair
+  metadata_type justMe(Node_CItr it) {
+    ll cnt = extract_value(it);
+    return {cnt, cnt * extract_key(it)};
+  }
+
+  // update metadata from left and right
+  void operator()(Node_Itr it, Node_CItr end_it) {
+    // account for itself
+    metadata_type x = justMe(it);
+
+    if (it.get_l_child() != end_it) // add a sum in left child:
+      x += it.get_l_child().get_metadata();
+    if (it.get_r_child() != end_it) // add a sum in right child:
+      x += it.get_r_child().get_metadata();
+
+    const_cast<metadata_type &>(it.get_metadata()) = x;
+  }
+
+  // remnant from inheriting from tree_order_statistics_node_update
+  int64_t order_of_key(int key) const {
+    throw invalid_argument("Don't call this anymore");
+  }
+
+  // {countGe, weightedSumLt}
+  metadata_type query(int ge) {
+    metadata_type accum = {0, 0};
+    query(node_begin(), ge, accum);
+    return accum;
+  }
+
+  virtual Node_CItr node_begin() const = 0; // pointer to the root
+  virtual Node_CItr node_end() const = 0;   // one past the last node
+
+private:
+  void query(Node_CItr it, int ge, metadata_type &accum) {
+    if (it == node_end()) {
+      return;
+    }
+
+    // we know that a red black tree is a valid bst
+    // so we only need to descend the tree once
+    // and this method is written tail recursively to better allow this
+    if (Cmp_Fn()((*it)->first, ge)) {
+      // if current number < ge, then all left child, and this
+      // need to get added to the weighted sum
+      // and then we recurse on the right
+      accum.second += justMe(it).second;
+      Node_CItr l_it = it.get_l_child();
+      if (l_it != node_end()) {
+        accum.second += l_it.get_metadata().second;
+      }
+      query(it.get_r_child(), ge, accum);
+    } else {
+      // current number >= ge, so this and all right child
+      // need to be added to count
+      accum.first += justMe(it).first;
+      Node_CItr r_it = it.get_r_child();
+      if (r_it != node_end()) {
+        accum.first += r_it.get_metadata().first;
+      }
+      query(it.get_l_child(), ge, accum);
+    }
+  }
 };
+using tree_ptr = __gnu_pbds::tree<int, ll, less<>, __gnu_pbds::rb_tree_tag,
+                                  custom_node_update>;
+using CustomTree = shared_ptr<tree_ptr>;
 
-const int RANGE_MAX = 1'000'002;
-const int RANGE_MIN = 0;
 struct Info {
-  DynamicSegtree stree;
-  map<int, ll> cnts;
+  CustomTree stree;
   int size;
-  Info() : stree(RANGE_MIN, RANGE_MAX), size(0) {}
+  Info() : stree(make_shared<tree_ptr>()), size(0) {}
 
   void insert(int num, ll cnt) {
-    cnts[num] += cnt;
-    stree.increment(num, cnt);
+    ll prevCnt = 0;
+    auto it = stree->find(num);
+    if (it != stree->end()) {
+      prevCnt = it->second;
+      stree->erase(it);
+    }
+
+    stree->insert({num, prevCnt + cnt});
     size += cnt;
   }
   void clampTo(int ma) {
-    while (!cnts.empty() && prev(cnts.end())->first > ma) {
-      auto it = prev(cnts.end());
+    while (!stree->empty() && prev(stree->end())->first > ma) {
+      auto it = prev(stree->end());
       auto [num, cnt] = *it;
       // erase previous
-      insert(num, -cnt);
-      cnts.erase(it);
+      size -= cnt;
+      stree->erase(it);
       // and insert ma
       insert(ma, cnt);
     }
@@ -126,7 +145,7 @@ int main() {
   ll total = 0;
   auto dfs = [&](auto &dfs, int i, int parent) -> Info {
     Info myInfo;
-    myInfo.insert(RANGE_MAX - 1, 1);
+    myInfo.insert(1'000'001, 1);
     for (auto [conn, speed] : g[i]) {
       if (conn != parent) {
         Info childInfo = dfs(dfs, conn, i);
@@ -136,16 +155,15 @@ int main() {
           swap(myInfo, childInfo);
         }
         // so now childInfo is the smaller one, insert that
-        for (auto [num, cnt] : childInfo.cnts) {
-          auto [countOfGe, _w] = myInfo.stree.query(num, RANGE_MAX);
-          auto [_c, weightedSumOfLt] = myInfo.stree.query(RANGE_MIN, num - 1);
+        for (auto [num, cnt] : *childInfo.stree) {
+          auto [countOfGe, weightedSumOfLt] = myInfo.stree->query(num);
           // num is the limiting factor for these
           total += countOfGe * cnt * num;
           // limiting factor is encoded in weighted sum
           total += weightedSumOfLt * cnt;
         }
         // and then actually do the merge
-        for (auto [num, cnt] : childInfo.cnts) {
+        for (auto [num, cnt] : *childInfo.stree) {
           myInfo.insert(num, cnt);
         }
       }
